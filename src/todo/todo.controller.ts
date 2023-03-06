c5447cf48b89b4e557ead40cfa1e82b52a030f13
mnt3710 <tomato3710@gmail.com>
2023-03-06 15:36:18 +0900

    post 動作確認 OK

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('todo')
 export class TodoController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('list')
-  getList() {
-    return [
-      {
-        title: '牛乳を買いに行く',
-        due_on: '2023-03-24',
-        done: false,
-      },
-    ];
+  async getList() {
+    const result = await this.prisma.task.findMany();
+    return [...result];
+  }
+
+  @Post('')
+  async add(@Body() task: CreateTaskDto) {
+    const result = await this.prisma.task.create({
+      data: task,
+    });
+    return {
+      status: 'OK',
+    };
   }
 }
